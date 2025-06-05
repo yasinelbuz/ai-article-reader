cb32f25f5f959144b760654d7580c82c56d57c66
Yasin Elbüz <yasinelbuz@gmail.com>
2025-06-05 17:53:00 +0300

    fix

--- components/article-card.tsx
+++ components/article-card.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import ReadingStatus from "@/components/reading-status";
 import dayjs from "dayjs";
 import { MoveRight } from "lucide-react";
 import relativeTime from "dayjs/plugin/relativeTime";
@@ -10,37 +9,33 @@ dayjs.extend(relativeTime);
 
 export default function ArticleCard({ article }: {article: ArticleTypes}) {
   return (
-    <div className="group relative block rounded-[20px] border border-gray-900 transition-all duration-300 hover:shadow-[0_0_50px_0_rgba(0,0,0,0.05)] overflow-hidden">
-      {/* Reading Status */}
-      <div className="absolute top-0 right-0 p-7 rounded-bl-full bg-gray-900">
-        <ReadingStatus articleId={article.id} />
-      </div>
+    <div className="h-[350px] text-black bg-white group relative block rounded-[20px] border border-gray-300 transition-all duration-300 hover:shadow-[0_0_50px_0_rgba(0,0,0,0.05)] overflow-hidden">
       <div
-        className={`group-hover:bg-blue-900 group-hover:p-32 transition-all duration-300 absolute top-0 left-0 p-24 rounded-br-full z-[0] ${
+        className={`group-hover:bg-blue-500 group-hover:p-36 transition-all duration-300 absolute top-0 left-0 p-24 rounded-br-full z-[0] ${
           article.category
         }`}
       ></div>
-      <div className="relative z-[1]">
+      <div className="h-full relative z-1">
         {/* Title & Description */}
         <div className="p-8">
           <Link href={`/articles/${generateSlug(article.category)}/${generateSlug(article.title)}`}>
-            <h3 className="text-[1.4rem] font-extrabold text-white leading-tight tracking-tight">
+            <h1 className="text-[2.8rem] font-extrabold leading-tight tracking-tight">
               {article.title}
-            </h3>
+            </h1>
           </Link>
 
           {/* Date */}
           <p className="text-gray-400 text-xs mt-1">{dayjs(article.created_time).fromNow()}</p>
 
           {/* Description */}
-          <p className="inline-block text-white line-clamp-2 mt-4 text-sm">
+          <p className="inline-block line-clamp-2 mt-4 text-sm">
             {article.content_summary}
           </p>
 
           {/* Reading Time & Difficulty Level */}
           <div className="flex items-center mt-4">
             <span
-              className={`inline-flex items-center px-3 py-1 border text-white text-xs font-semibold rounded-full capitalize
+              className={`inline-flex items-center px-3 py-1 border text-xs font-semibold rounded-full capitalize
                 ${article.category === "Beginner" ? "border-green-600" : ""}
                 ${article.category === "Intermediate" ? "border-yellow-600" : ""}
                 ${article.category === "Advanced" ? "border-red-600" : ""}`}
@@ -54,7 +49,7 @@ export default function ArticleCard({ article }: {article: ArticleTypes}) {
         {/* Footer */}
         <Link
           href={`/articles/${generateSlug(article.category)}/${generateSlug(article.title)}`}
-          className="hover:bg-white/5 border-t border-gray-900 block"
+          className="border-t border-gray-900 block bg-black absolute left-0 right-0 bottom-0"
         >
           <div className="p-4 sticky bottom-0 flex items-center justify-center">
             <span className="inline-flex items-center gap-3 text-sm font-semibold text-gray-100 transition-colors">
--- containers/home/index.tsx
+++ containers/home/index.tsx
@@ -8,6 +8,7 @@ import { ArticleTypes } from "@/types/articles";
 import { cn } from "@/utils";
 import { Divider } from "@/components/ui/divider";
 import { siteText } from "@/config/site";
+import { generateSlug } from "@/utils/slug-generator";
 
 
 type FilterButtonTypes = {
@@ -27,7 +28,7 @@ type HomeContainerTypes = {
 const initialSelectedLevelAndReadStatusValue = "all"
 
 const mainClass = "container mx-auto px-4 py-16"
-const mainInlineClass = "flex flex-col gap-4"
+const mainInlineClass = "flex flex-col gap-4 my-8"
 const dividerClass = "my-8 border-gray-600"
 const articleCountClass = "flex items-center text-sm text-gray-400"
 const articleGridClass = "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
@@ -38,21 +39,19 @@ export default function HomeContainer({ articles }: HomeContainerTypes) {
 
   const filteredArticles:ArticleTypes[] = articles.filter((article: ArticleTypes) => {
     if (selectedLevel === initialSelectedLevelAndReadStatusValue) return true;
-    return article.category === selectedLevel;
+    return generateSlug(article.category) === selectedLevel;
   });
 
   return (
     <main className={mainClass}>
       <div className={mainInlineClass}>
         <FilterButtons selectedLevel={selectedLevel} setSelectedLevel={setSelectedLevel}/>
-        <ReadStatusButtons selectedReadStatus={readStatus} setSelectedReadStatus={setReadStatus}/>
+        {/* <ReadStatusButtons selectedReadStatus={readStatus} setSelectedReadStatus={setReadStatus}/> */}
 
         <div className={articleCountClass}>
           {filteredArticles.length} {siteText.home.articleCountLabel}
         </div>
       </div>
-
-      <Divider className={dividerClass} />
 
       <div className={articleGridClass}>
         {filteredArticles.map((article: ArticleTypes) => (

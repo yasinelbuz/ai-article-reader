40c35e6611cf30d7f608ff4567ce0045e6c6284f
Yasin Elbüz <yasinelbuz@gmail.com>
2025-06-11 23:50:51 +0300

    fix

--- containers/post/index.tsx
+++ containers/post/index.tsx
@@ -23,27 +23,17 @@ export default function ContainerPost({ article, category }: ContainerPostProps)
     <div className="md:p-16 p-6">
       <Button
         className="flex items-center gap-2 mb-2"
-        variant="alternative"
+        variant="gradientTealLime"
         onClick={() => navigate.back()}
       >
         <ArrowLeft />
         <span>Back</span>
       </Button>
 
-      <div className="flex items-center gap-2 mb-4 ">
-        <span className="first-letter:uppercase text-sm tracking-widest text-gray-400">
-          {category}
-        </span>
-
-        <span className="text-sm text-gray-400">
-          {siteText.post.readingTime} {calculateReadingTime(article?.content)}
-        </span>
-      </div>
-
-      <Divider className="mb-12" />
-
       <div>
-        <h1 className="text-3xl md:text-7xl font-extrabold mb-6 leading-snug">{article?.title}</h1>
+        <h1 className="text-3xl md:text-7xl/[6rem] font-extrabold mb-6 text-purple-700">
+          {article?.title}
+        </h1>
 
         <p className="text-lg md:text-4xl/[3.5rem] whitespace-pre-line">
           <SpeechText article={article?.content} />

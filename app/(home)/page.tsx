c32b1bb794afea9ca11f3273943807ede543cd1e
Yasin Elbüz <yasinelbuz@gmail.com>
2025-07-06 22:15:44 +0300

    Home bileşeninde dil parametresi güncellendi, artık searchParams üzerinden alınmakta ve varsayılan değer olarak 'english' kullanılmakta.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,8 +2,8 @@ import HomeContainer from '@/containers/home';
 import { getPosts } from '@/services/articles';
 import { ArticleTypes } from '@/types/articles';
 
-export default async function Home({ params }: { params: Promise<{ language: string }> }) {
-  const [language] = (await params).language || [];
+export default async function Home({ searchParams }: { searchParams: Promise<{ language: string }> }) {
+  const language = (await searchParams).language || 'english';
   const articles: ArticleTypes[] = await getPosts(language);
 
   return <HomeContainer articles={articles} />;
